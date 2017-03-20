f77b588de20112c77bf2e6ab1776fa9e6d57157c
Jannes Höke <mail@jannes-hoeke.de>
2017-03-20 14:09:03 +0100

    Abfrage von bestimmten Straßen funktioniert

--- stauschau.py
+++ stauschau.py
@@ -1,30 +1,50 @@
 import logging
+import json
 
 from flask import Flask
 from flask_ask import Ask, request, session, question, statement
+import requests
 
 
 app = Flask(__name__)
 ask = Ask(app, "/")
+#logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+
+
+def get_traffic_messages():
+    logging.info('Downloading traffic info JSON...')
+    r = requests.get('http://exporte.wdr.de/WDRVerkehrWebsite/map/all?zoom=8&bbox=4.27%2C49.71%2C12.73%2C53.01',)
+                     #params={'zoom': '8', 'bbox': '4.27 49.71 12.73 53.01'})
+
+    logging.info('Parsing JSON...')
+    response_dict = json.loads(r.text)
+
+    return response_dict['messages']
 
 
 @ask.launch
 def launch():
-    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    speech_text = 'Nennen Sie eine Strecke, zum Beispiel "A1"'
+    return question(speech_text).reprompt(speech_text)
 
 
-@ask.intent('HelloWorldIntent')
-def hello_world():
-    speech_text = 'Hello world'
-    return statement(speech_text).simple_card('HelloWorld', speech_text)
+@ask.intent('QueryIntent')
+def query(road_type, road_number):
+    road = road_type + road_number
+    logging.info('Query for %s', road)
+    messages_for_road = [message['description'].replace('<br />', '\n')
+                         for message in messages
+                         if message['road'] == road]
+
+    speech_text = '\n\n'.join(messages_for_road)
+    return statement(speech_text).simple_card('WDR StauSchau', speech_text)
 
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
-    speech_text = 'You can say hello to me!'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    speech_text = 'Liefert aktuelle Verkehrsinformationen des WDR'
+    return question(speech_text).reprompt(speech_text)
 
 
 @ask.session_ended
@@ -33,4 +53,5 @@ def session_ended():
 
 
 if __name__ == '__main__':
+    messages = get_traffic_messages()
     app.run(debug=True)
